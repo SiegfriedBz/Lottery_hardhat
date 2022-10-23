548da1e0a0ba31cf0e934bc7c0700e8f79ffef6e
siegfried bozza <siegfried.bozza@protonmail.com>
2022-10-23 12:01:58 +0200

    Update unit tests

--- test/unit/lottery.test.js
+++ test/unit/lottery.test.js
@@ -11,14 +11,15 @@ require("hardhat-gas-reporter")
 !developmentChains.includes(network.name)
     ? describe.skip
     : describe("Lottery", function () {
-          let deployer, user01
+          let accounts, deployer, user01
           let lottery
           let VRFCoordinatorV2Mock
           let entranceFee, interval
           const chainId = network.config.chainId
 
           beforeEach(async function () {
-              ;[deployer, user01] = await ethers.getSigners()
+              accounts = await ethers.getSigners()
+              ;[deployer, user01] = accounts
               await deployments.fixture(["all"])
               VRFCoordinatorV2Mock = await ethers.getContract(
                   "VRFCoordinatorV2Mock",
@@ -208,13 +209,6 @@ require("hardhat-gas-reporter")
                       // call performUpkeep
                       let transactionResponse = await lottery.performUpkeep([])
                       let transactionReceipt = await transactionResponse.wait(1)
-                      // assert lottery's latestTimeStamp is updated
-                      let { blockNumber } = transactionReceipt
-                      let expectedTimeStamp = (
-                          await ethers.provider.getBlock(blockNumber)
-                      ).timestamp
-                      let latestTimeStamp = await lottery.getLatestTimeStamp()
-                      expect(latestTimeStamp).to.equal(expectedTimeStamp)
                       // assert lottery's state is updated to CALCULATING
                       let lotteryState = await lottery.getLotteryState()
                       expect(lotteryState.toString()).to.equal("1")
@@ -227,7 +221,6 @@ require("hardhat-gas-reporter")
                       /// note: we check events[1] bcz VRFCoordinator already emitted an event (RandomWordsRequested) (events[0]) by calling requestRandomWords(//)
                       let requestId =
                           transactionReceipt.events[1].args.requestId
-                      console.log("requestId", requestId)
                       expect(requestId.toNumber() > 0)
                   })
               })
@@ -258,6 +251,7 @@ require("hardhat-gas-reporter")
 
               it("reverts if performUpkeep was not called before", async function () {
                   // after request for randomness is made, ChainLink nodes run off-chain their own fulfillRandomWords, and if a request was actually made it ends up by the ChainLink nodes calling our fulfillRandomWords, on-chain
+                  /// we pass a requestId and our contract address for ChainLink nodes to sent us back the random number if request successful.
                   await expect(
                       VRFCoordinatorV2Mock.fulfillRandomWords(
                           0,
@@ -272,6 +266,94 @@ require("hardhat-gas-reporter")
                   ).to.be.revertedWith("nonexistent request")
               })
 
-              it("sets the winner, updates the lottery state, and sends value to winner", async function () {})
+              it("sets the winner, reset the lottery, and sends value to winner", async function () {
+                  // add players and store initial Balances (after entering lottery, before winner pick up)
+                  const deployer_Balance_Initial =
+                      await ethers.provider.getBalance(deployer.address)
+                  let players_Balances_Initial = []
+                  players_Balances_Initial.push(deployer_Balance_Initial)
+                  for (let i = 1; i < 4; i++) {
+                      const player = accounts[i]
+                      await lottery
+                          .connect(player)
+                          .enterLottery({ value: entranceFee })
+                      let player_Balance_Initial =
+                          await ethers.provider.getBalance(player.address)
+                      players_Balances_Initial.push(player_Balance_Initial)
+                  }
+                  const players = await lottery.getPlayers()
+                  expect(players.length).to.equal(4)
+
+                  //
+                  const StartingTimeStamp = await lottery.getLatestTimeStamp()
+                  console.log(
+                      new Date(StartingTimeStamp * 1000).toLocaleString()
+                  )
+                  // performUpkeep (mock being ChainLink Keepers)
+                  // fulfillRandomWords (mock being ChainLink VRF)
+                  // we will have to wait for the fulfillRandomWords to be called
+                  await new Promise(async (resolve, reject) => {
+                      // set up listener
+                      lottery.once("WinnerPicked", async () => {
+                          /// try catch block. note: mocha timeout set up in hh config
+                          console.log("WinnerPicked event was emitted at ")
+                          try {
+                              const winner = await lottery.getNewWinner()
+                              let indexOfWinner
+                              for (let account of accounts) {
+                                  if (account.address == winner) {
+                                      indexOfWinner = accounts.indexOf(account)
+                                  }
+                              }
+                              const winner_Balance =
+                                  await ethers.provider.getBalance(winner)
+                              const contractBalance =
+                                  await ethers.provider.getBalance(
+                                      lottery.address
+                                  )
+                              const prize = await lottery.getNewWinnerPrize()
+                              const endingTimeStamp =
+                                  await lottery.getLatestTimeStamp()
+                              console.log(
+                                  new Date(
+                                      endingTimeStamp * 1000
+                                  ).toLocaleString()
+                              )
+                              const lotteryState =
+                                  await lottery.getLotteryState()
+                              const newPlayers = await lottery.getPlayers()
+                              expect(players).to.include(winner)
+                              expect(winner_Balance).to.equal(
+                                  players_Balances_Initial[indexOfWinner].add(
+                                      prize
+                                  )
+                              )
+                              expect(contractBalance.toString()).to.equal("0")
+                              expect(prize).to.equal(
+                                  entranceFee.mul(players.length)
+                              )
+                              expect(endingTimeStamp).to.be > StartingTimeStamp
+                              expect(lotteryState.toString()).to.equal("0")
+                              expect(newPlayers.length.toString()).to.equal("0")
+                          } catch (error) {
+                              console.log(error)
+                              reject(error)
+                          }
+                          resolve()
+                      })
+
+                      // set up listener
+
+                      // fire the event, listener will pick it up and resolve
+                      /// performUpkeep (mock being ChainLink Keepers calling our performUpkeep). Note : players added, funds added, time
+                      const tx = await lottery.performUpkeep([])
+                      const txReceipt = await tx.wait(1)
+                      /// fulfillRandomWords (mock being ChainLink VRF calling their fulfillRandomWords to which we pass the requestId got from the performUpkeep call + lottery contract address to send us back the random number
+                      await VRFCoordinatorV2Mock.fulfillRandomWords(
+                          txReceipt.events[1].args.requestId,
+                          lottery.address
+                      )
+                  })
+              })
           })
       })
